89752db7c296cfcaff3b97fb83afc86e32afdac2
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2021-06-24 14:15:30 -0300

    Appointments Header finished

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,6 @@
 import React,{ useState } from 'react';
 import { View, FlatList, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { Profile } from '../../components/Profile';
 import { ButtonAdd } from '../../components/ButtonAdd';
@@ -13,6 +14,7 @@ import { Appointment } from '../../components/Appointment';
 
 export function Home() {
   const [category, setCategory] = useState('');
+  const  navigation = useNavigation();
 
   const appointments = [
     {
@@ -45,6 +47,10 @@ export function Home() {
     categoryId === category ? setCategory('') : setCategory(categoryId);
   }
 
+  function handleAppointmentsDetails(){
+    navigation.navigate('AppointmentDetails');
+  }
+
   return(
     <Background >
       <View style={styles.header}>
@@ -67,7 +73,10 @@ export function Home() {
             data={appointments}
             keyExtractor={item => item.id}
             renderItem={({ item }) => (
-              <Appointment data={item} />
+              <Appointment 
+                data={item} 
+                onPress={handleAppointmentsDetails}
+              />
             )}
             ItemSeparatorComponent={() => <ListDivider />}
             style={styles.matches}
