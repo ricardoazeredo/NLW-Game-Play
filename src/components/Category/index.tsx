2ef9b684d67ab778de9078d0f88e337c5e2f9cd7
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2021-06-23 21:51:37 -0300

    Updating Category cards: background and check option button

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -11,16 +11,23 @@ import { SvgProps } from 'react-native-svg';
 type Props = RectButtonProps & {
   title: string;
   icon: React.FC<SvgProps>;
+  hasCheckedBox?: boolean;
   checked?: boolean;
 }
 
 export function Category({
   title,
   icon: Icon,
-  checked = true,
+  checked = false,
+  hasCheckedBox = false,
   ...rest
 }: Props) {
-  const { secondary50, secondary70} = theme.colors;
+  const { 
+    secondary40,
+    secondary50, 
+    secondary70,
+    secondary85
+  } = theme.colors;
 
   return (
     <RectButton {...rest}>
@@ -28,11 +35,17 @@ export function Category({
         style={styles.container}
         colors={[secondary50, secondary70]}
       >
-        <View style={[styles.content, { opacity: checked ? 1 : 0.4 }]}>
-          <View style={
-              checked ? styles.checked : styles.check 
-            } 
-          />
+        <LinearGradient 
+          style={[styles.content, { opacity: checked ? 1 : 0.5 }]}
+          colors={[checked ? secondary85 : secondary50, secondary40]}
+        >
+          {
+            hasCheckedBox && 
+            <View style={
+                checked ? styles.checked : styles.check 
+              } 
+            />
+          }
           
           <Icon 
             width={48} 
@@ -43,7 +56,7 @@ export function Category({
             { title }
           </Text>
 
-        </View>
+        </LinearGradient>
       </LinearGradient>
 
     </RectButton>    
